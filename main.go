12a18238472d298669bcce17b5879f348515f902
mlemesle <lemesle.martin@hotmail.fr>
2020-11-28 18:30:09 +0100

    main now accepts user input parameters

--- main.go
+++ main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"flag"
+	"github.com/mlemesle/chip-go-8/lib/beeper"
 	"github.com/mlemesle/chip-go-8/lib/emulator"
 	"github.com/mlemesle/chip-go-8/lib/screen"
-	"github.com/veandco/go-sdl2/sdl"
 	"os"
 )
 
 func main() {
-	chip8 := emulator.New()
-	chip8.Initialize()
-	err := chip8.LoadMemory("rom/pong.c8")
-	if err != nil {
-		panic(err)
-	}
+	ratio := flag.Int("ratio", 20, "The ratio of the screen. The screen is 64x32. Default value is 20")
+	isMuted := flag.Bool("mute", false, "The emulator will be muted if set.")
+	runTest := flag.Bool("test", false, "If set, the emulator will boot with the test chip8 image from https://github.com/corax89/chip8-test-rom")
+	romFile := flag.String("rom", "rom/pong.c8", "Specify a rom file to run. If not set, a pong image wille be loaded")
+	flag.Parse()
 
-	chip8ScreenSDL := screen.NewChip8ScreenSDL(64, 32, 20)
-	err = chip8ScreenSDL.Init()
+	chip8ScreenSDL := screen.NewChip8ScreenSDL(64, 32, int32(*ratio))
+	err := chip8ScreenSDL.Init()
 	if err != nil {
 		panic(err)
 	}
 	defer chip8ScreenSDL.Destroy()
+
+	var chip8Beeper beeper.BeeperInterface
+	if *isMuted {
+		chip8Beeper = beeper.NewMute()
+	} else {
+		chip8Beeper = beeper.NewSDL()
+	}
+	err = chip8Beeper.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer chip8Beeper.Destroy()
+
+	chip8 := emulator.New()
+	chip8.Initialize(chip8Beeper)
+	if *runTest {
+		*romFile = "rom/test_opcode.ch8"
+	}
+	err = chip8.LoadMemory(*romFile)
+	if err != nil {
+		panic(err)
+	}
 
 	for {
 		if err = chip8.EmulateCycle(); err != nil {
@@ -37,6 +59,5 @@ func main() {
 		if quitEvent {
 			os.Exit(0)
 		}
-		sdl.Delay(1000 / 1000)
 	}
 }
